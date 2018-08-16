3c30a72410fc03482927a5a704fc69303974ab7a
Adam Zielonka <root@adamzielonka.pro>
2018-08-16 20:55:23 +0200

    Possible actions and winner check

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,77 +1,26 @@
 import * as React from 'react'
+import { Colors, Filds, PlayerFilds } from './Consts'
 import Dice from './Dice'
 import './Game.css'
-
-
-const Colors = [
-  'empty', 'green', 'yellow', 'red', 'blue'
-]
-
-const Filds = [
-  [3, 3,  ,  , 0, 0, 4,  ,  , 4, 4, ],
-  [3, 3,  ,  , 0, 4, 0,  ,  , 4, 4, ],
-  [ ,  ,  ,  , 0, 4, 0,  ,  ,  ,  , ],
-  [ ,  ,  ,  , 0, 4, 0,  ,  ,  ,  , ],
-  [3, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, ],
-  [0, 3, 3, 3, 3, 0, 1, 1, 1, 1, 0, ],
-  [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, ],
-  [ ,  ,  ,  , 0, 2, 0,  ,  ,  ,  , ],
-  [ ,  ,  ,  , 0, 2, 0,  ,  ,  ,  , ],
-  [2, 2,  ,  , 0, 2, 0,  ,  , 1, 1, ],
-  [2, 2,  ,  , 2, 0, 0,  ,  , 1, 1, ]
-]
-
-const PlayerFilds = []
-
-PlayerFilds[1] = {
-  home: [[9, 5], [8, 5], [7, 5], [6, 5]],
-  board: [[10, 6], [9, 6], [8, 6], [7, 6], [6, 6], [6, 7], [6, 8], [6, 9], [6, 10], [5, 10]],
-  start: [[9, 9], [10, 9], [9, 10], [10, 10]],
-  order: [1, 2, 3, 4]
-}
-
-PlayerFilds[2] = {
-  home: [[5, 9], [5, 8], [5, 7], [5, 6]],
-  board: [[4, 10], [4, 9], [4, 8], [4, 7], [4, 6], [3, 6], [2, 6], [1, 6], [0, 6], [0, 5]],
-  start: [[0, 9], [1, 9], [0, 10], [1, 10]],
-  order: [2, 3, 4, 1]
-}
-
-PlayerFilds[3] = {
-  home: [[1, 5], [2, 5], [3, 5], [4, 5]],
-  board: [[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [4, 3], [4, 2], [4, 1], [4, 0], [5, 0]],
-  start: [[0, 0], [1, 0], [0, 1], [1, 1]],
-  order: [3, 4, 1, 2]
-}
-
-PlayerFilds[4] = {
-  home: [[5, 1], [5, 2], [5, 3], [5, 4]],
-  board: [[6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [10, 5]],
-  start: [[9, 0], [10, 0], [9, 1], [10, 1]],
-  order: [4, 1, 2, 3]
-}
-
-const PlayerBoard = [,
-  [[9, 9], [10, 9], [9, 10], [10, 10]],
-  [[0, 9], [1, 9], [0, 10], [1, 10]],
-  [[0, 0], [1, 0], [0, 1], [1, 1]],
-  [[9, 0], [10, 0], [9, 1], [10, 1]]
-]
-
 
 interface ISquareProps {
     color: number
     activeColor: number
+    disabled: boolean
     player?: number
+    x: number
+    y: number
     onClick(): void
 }
 
 function Square(props: ISquareProps) {
   const color = (props.color === undefined ? '' : 'bg-' + Colors[props.color])
   const stone = (<div className={'p-' + Colors[props.activeColor]}>&#11044;</div>)
+  const numbers = '' // (<div className='numbers'>{props.x} {props.y}</div>)
   return (
-      <button className={'square ' + (color)} onClick={props.onClick}>
-        {props.activeColor ? stone : ' '}
+      <button className={'square ' + (color)} onClick={props.onClick} disabled={props.disabled}>
+        {props.activeColor ? stone : props.color !== undefined ? numbers : ''}
+        {/* {props.color !== undefined ? numbers : ''} */}
       </button>
   )
 }
@@ -79,7 +28,8 @@ function Square(props: ISquareProps) {
 interface IGameState {
   filds: number[][],
   token: number,
-  dice: Dice
+  dice: Dice,
+  winner: number
 }
 
 export default class Game extends React.Component<null, IGameState> {
@@ -87,6 +37,7 @@ export default class Game extends React.Component<null, IGameState> {
   constructor(props) {
     super(props)
     this.state = {
+      winner: 0,
       token: 1,
       dice: new Dice(),
       filds: [
@@ -177,36 +128,110 @@ export default class Game extends React.Component<null, IGameState> {
   }
 
   public nextPlayer(this) {
-    this.state.dice.throw()
-    let token = this.state.token + 1
-    if (token > 4) { token = 1 }
-    this.setState({ token })
+    const winner = this.checkWiner()
+    if (winner) {
+      this.setState({ winner })
+    } else {
+      this.state.dice.throw()
+      let token = this.state.token + 1
+      if (token > 4) { token = 1 }
+      this.setState({ token })
+    }
+  }
+
+  public posibleActions() {
+    const fields = this.state.filds
+    const token = this.state.token
+    const count = this.state.dice.state
+    const result = []
+
+    // Start field
+    if (count === 6) {
+      for (const field of PlayerFilds[token].start) {
+        const [x, y] = PlayerFilds[token].board[0]
+        if (fields[field[1]][field[0]] === token && fields[y][x] !== token) {
+          result.push(field)
+        }
+      }
+    }
+
+    // Set path
+    let board = []
+    for (const id of PlayerFilds[token].order) {
+      board = board.concat(PlayerFilds[id].board)
+    }
+    board = board.concat(PlayerFilds[token].home)
+
+    // Find next field
+    let i = 0
+    for (const field of board) {
+      if (fields[field[1]][field[0]] === token) {
+        if (board.length - 1 >= i + count && fields[board[i + count][1]][board[i + count][0]] !== token) {
+          result.push(field)
+        }
+      }
+      ++i
+    }
+    return result
+  }
+
+  public checkWiner(): number {
+    const fields = this.state.filds
+    let playerID = 0
+    for (const player of PlayerFilds) {
+      if (player) {
+        ++playerID
+        let counter = 0
+        for (const field of player.home) {
+          if (fields[field[1]][field[0]] === playerID) { ++counter }
+        }
+        if (counter === 4) { return playerID }
+      }
+    }
+    return 0
   }
 
   public render() {
     const filds = this.state.filds
+    const actions = this.posibleActions()
+    const winner = this.state.winner
     const row = []
     let squares = []
     for (let i = 0; i < Filds.length; i++) {
       for (let j = 0; j < Filds[i].length; j++) {
-        squares.push(<Square key={`${i}-${j}`} color={Filds[i][j]} activeColor={filds[i][j]} onClick={this.onClick.bind(this, i, j)}/>)
+        squares.push(<Square key={`${i}-${j}`} x={i} y={j} color={Filds[i][j]}
+            activeColor={filds[i][j]} onClick={this.onClick.bind(this, i, j)} disabled={Boolean(winner)}/>)
       }
       row.push(<div key={`r-${i}`} className='board-row'>{squares}</div>)
       squares = []
     }
+
+    const gameInfo = !winner ? (
+      <div className='game-info'>
+        <div className={'bg-' + Colors[this.state.token]}>Player {this.state.token} - {Colors[this.state.token].toUpperCase()}</div>
+        <div>Dice: {this.state.dice.state}</div>
+        <div>Winner: {winner}</div>
+        <button onClick={this.noMoves.bind(this, this)} hidden={Boolean(actions.length)}>
+          No moves
+        </button>
+        {actions.map(action => (
+          <button key={'a' + action[1] + '-' + action[0]} onClick={this.onClick.bind(this, action[1], action[0])}>
+            {action[1]} {action[0]}
+          </button>
+        ))}
+      </div>
+    ) : (
+      <div className='game-info'>
+        <div className={'bg-' + Colors[winner]}>Winner: Player {winner} - {Colors[winner].toUpperCase()}</div>
+      </div>
+    )
 
     return (
       <div className='game'>
         <div className='game-board'>
           {row}
         </div>
-        <div className='game-info'>
-            <div className={'bg-' + Colors[this.state.token]}>Player {this.state.token} - {Colors[this.state.token].toUpperCase()}</div>
-            <div>Dice: {this.state.dice.state}</div>
-            <button onClick={this.noMoves.bind(this, this)}>
-              No moves
-            </button>
-          </div>
+        {gameInfo}
       </div>
     )
   }
