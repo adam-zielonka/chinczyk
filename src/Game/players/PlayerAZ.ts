9c0f7cc87d9772f7a465bcb7a32832290913d8e1
Adam Zielonka <root@adamzielonka.pro>
2018-08-30 22:33:53 +0200

    fieldsOfDestruction - Begining

--- src/Game/players/PlayerAZ.ts
+++ src/Game/players/PlayerAZ.ts
@@ -1,11 +1,37 @@
+import { PlayerFilds } from '../Consts'
 import Game from '../Game'
 import Player from './Player'
 
 export default class PlayerAZ extends Player {
 
+  // fieldsOfDestruction(game: Game) {
+  //   const fields = game.getState().filds
+  //   const token = game.getState().token
+  //   const count = game.getState().dice.state
+  //   let board = []
+  //   for (const id of PlayerFilds[token].order) {
+  //     board = board.concat(PlayerFilds[id].board)
+  //   }
+  //   board = board.concat(PlayerFilds[token].home)
+
+  //   const stones = []
+
+  //   let i = 0
+  //   const result = board.map(field => {
+  //     const fieldValue = fields[field[1]][field[0]]
+  //     if (fieldValue !== token && fieldValue !== 0) {
+  //       stones.push(field)
+  //     }
+  //     ++i
+  //     return 0
+  //   })
+  //   return result
+  // }
+
   play(game: Game) {
     super.play(game)
     const actions = game.posibleActions()
+    // console.log(this.fieldsOfDestruction(game))
     switch (actions.length) {
       case 0:
         game.noMoves()
